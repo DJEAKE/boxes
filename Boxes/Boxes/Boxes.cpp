b641a01477e89194b8b2aedafa1c4cafc5754354
DJEAKE <djeakee@gmail.com>
2023-09-13 21:10:31 +0800

    Written program

--- Boxes/Boxes/Boxes.cpp
+++ Boxes/Boxes/Boxes.cpp
@@ -1,20 +1,21 @@
-﻿// Boxes.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
+﻿#include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "russian");
+
+	int transferringBallsToStore = 10;
+	int countBallsInStore = 0;
+	int transferringBallsToCar = 0;
+
+	std::cout << "На складе " << countBallsInStore << " шаров" << std::endl;
+	countBallsInStore = transferringBallsToStore;
+
+	std::cout << "На склад загружено " << countBallsInStore << " шаров" << std::endl;
+	transferringBallsToCar = countBallsInStore;
+
+	std::cout << "В грузовую машину загружено " << transferringBallsToCar << " шаров" << std::endl;
+	countBallsInStore = 0;
+
+	std::cout << "На складе " << countBallsInStore << " шаров" << std::endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
